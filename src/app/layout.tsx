ccc604ffe093472e9abbfb1612ad3d54848912c3
asmanGoBim <muhasmangit@gmail.com>
2025-03-15 11:58:04 +0700

    update title and logo to reflect new personal branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ import Footer from '@/components/layout/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
-const title = 'Sagar Shah | Full Stack Developer From Ahmedabad, India.';
+const title = 'Muhammad Asman | Full Stack Developer From Yogyakarta, Indonesia.';
 const description =
   'A self-proclaimed designer who specializes in full stack development (React.js & Node.js), from Ahmedabad, India.';
 const url = 'https://sagarshah.dev';
