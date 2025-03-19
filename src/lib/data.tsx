d7e5d0d5de270409ef2eaa42155b0b07486d578c
muhammadasman <muhasmangit@gmail.com>
2025-03-19 19:53:17 +0700

    feat: update technologies for the Fiskil project to include GO Lang, Nest JS, PostgreSQL, REST API, and GRPC

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -309,8 +309,11 @@ export const PROJECTS: ProjectDetails[] = [
     url: '#',
     previewImage: ProjectFiskil,
     technologies: [
-      'Odoo',
-      'Python',
+      'GO Lang',
+      'Nest JS',
+      'PostgreSQL',
+      'REST API',
+      'GRPC',
     ],
   },
   {
