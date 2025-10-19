690aebe2fb8f442da6decb15b619755f89408564
muhammadasman <muhasmangit@gmail.com>
2025-10-19 19:29:45 +0700

    fix: handle download cv with new path

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -3,10 +3,8 @@
 import Button from '@/components/general/button';
 
 const DownloadCV = () => {
-  const basePath = '/infomee';
-
   return (
-    <Button onClick={() => window?.open(`${basePath}/files/CV-Muhammad Asman-Software Engineer.pdf`, '_blank')}>
+    <Button onClick={() => window?.open('/files/CV-Muhammad Asman-Software Engineer.pdf', '_blank')}>
       Download CV
     </Button>
   );
